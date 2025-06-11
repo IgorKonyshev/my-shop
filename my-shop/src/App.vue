0332ba24a7a5a00060a19a52df0e978995bf33a2
IgorKonyshev <snpport123@gmail.com>
2025-06-11 20:59:18 +0300

    vue new object OMG

---
+++ my-shop/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <ProductList />
+  </div>
+</template>
+
+<script>
+import ProductList from "my-shop/src/components/ProductList.vue";
+export default{
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
